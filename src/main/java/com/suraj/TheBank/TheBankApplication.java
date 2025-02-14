98a2f621ea5b9c7b0d70361c02f80c41e8e13a9b
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-02-14 10:26:09 +0530

    initial commit

---
+++ src/main/java/com/suraj/TheBank/TheBankApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.TheBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suraj/TheBank/TheBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.TheBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
